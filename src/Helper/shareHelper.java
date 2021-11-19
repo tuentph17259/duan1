084f8ee574b207121537884f9006a39e3e9f0fce
tuentph17259 <tuentph17259@fpt.edu.vn>
2021-11-19 20:50:47 +0700

    dn2

--- src/Helper/shareHelper.java
+++ src/Helper/shareHelper.java
@@ -22,35 +22,35 @@ import javax.swing.ImageIcon;
 public class shareHelper {
 
     //Ảnh biểu tượng của ứng dụng, xuất hiện trên mọi cửa sổ
-    public static final Image AppIcon;
-    public static final ImageIcon AppIcon1;
-
-    static {
-        String file = "/icon/dt.png";
-        AppIcon = new ImageIcon(shareHelper.class.getResource(file)).getImage();
-        AppIcon1 = new ImageIcon(shareHelper.class.getResource(file));
-    }
-
-    //Sao chép file logo chuyên đề vào thư mục logos (tạo nếu chưa có thư mục logos)
-    public static void save(File src) {
-        File dst = new File("logos", src.getName());
-        if (!dst.getParentFile().exists()) {
-            dst.getParentFile().mkdirs();//Tạo thư mục
-        }
-        try {
-            Path from = Paths.get(src.getAbsolutePath());
-            Path to = Paths.get(dst.getAbsolutePath());
-            Files.copy(from, to, StandardCopyOption.REPLACE_EXISTING);
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
-    }
-
-    //Đọc hình ảnh logo chuyên đề trong thư mục logos theo tenFile
-    public static ImageIcon readLogo(String fileName) {
-        File path = new File("logos", fileName);
-        return new ImageIcon(new ImageIcon(path.getAbsolutePath()).getImage().getScaledInstance(180, 180, Image.SCALE_DEFAULT));
-    }
+//    public static final Image AppIcon;
+//    public static final ImageIcon AppIcon1;
+//
+//    static {
+//        String file = "/icon/dt.png";
+//        AppIcon = new ImageIcon(shareHelper.class.getResource(file)).getImage();
+//        AppIcon1 = new ImageIcon(shareHelper.class.getResource(file));
+//    }
+//
+//    //Sao chép file logo chuyên đề vào thư mục logos (tạo nếu chưa có thư mục logos)
+//    public static void save(File src) {
+//        File dst = new File("logos", src.getName());
+//        if (!dst.getParentFile().exists()) {
+//            dst.getParentFile().mkdirs();//Tạo thư mục
+//        }
+//        try {
+//            Path from = Paths.get(src.getAbsolutePath());
+//            Path to = Paths.get(dst.getAbsolutePath());
+//            Files.copy(from, to, StandardCopyOption.REPLACE_EXISTING);
+//        } catch (Exception ex) {
+//            throw new RuntimeException(ex);
+//        }
+//    }
+//
+//    //Đọc hình ảnh logo chuyên đề trong thư mục logos theo tenFile
+//    public static ImageIcon readLogo(String fileName) {
+//        File path = new File("logos", fileName);
+//        return new ImageIcon(new ImageIcon(path.getAbsolutePath()).getImage().getScaledInstance(180, 180, Image.SCALE_DEFAULT));
+//    }
     //Đối tượng này chứa thông tin người sử dụng sau khi đăng nhập
     public static User user = null;
 

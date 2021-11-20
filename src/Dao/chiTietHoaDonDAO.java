47fb9c377388ecd6bf222683af864b30c148865e
tuentph17259 <94522493+tuentph17259@users.noreply.github.com>
2021-11-20 23:28:11 +0700

    Duchvph17480 hoa don dao hoa don chi tiet dao (#8)
    * ducph17480 - vs1
    
    * vs 2
    
    * vs2
    
    * vs2
    
    Co-authored-by: Duchvph17480 <duchvph17480@fpt.edu.vn>

---
+++ src/Dao/chiTietHoaDonDAO.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dao;
+
+import Helper.jdbcHelper;
+import Model.chiTietHoaDon;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author acer
+ */
+public class chiTietHoaDonDAO extends cuaHangDao<chiTietHoaDon, String>{
+    String INSERT_SQL = "INSERT INTO CHITIETHOADON (MACHITIET,MAHD,SOLUONG,DONGIA,TONGTIEN,MASP) VALUES (?,?,?,?,?,?)";
+    String UPDATE_SQL = "UPDATE CHITIETHOADON SET MAHD=?,SOLUONG=?,DONGIA=?,TONGTIEN=? , MASP=? where MACHITIET=?";
+    String DELETE_SQL = "DELETE FROM CHITIETHOADON where MACHITIET=?";
+    String SELECT_ALL_SQL = "SELECT * FROM CHITIETHOADON";
+    String SELECT_BY_ID_SQL = "SELECT * FROM CHITIETHOADON WHERE MACHITIET = ?";
+
+    @Override
+    public void insert(chiTietHoaDon entity) {
+        jdbcHelper.executeUpdate(INSERT_SQL, entity.getMaChiTietHoaDon(),entity.getMaHoaDon(),entity.getSoLuong(),entity.getDonGia(),entity.getTongTien(),entity.getMaSanPham());
+    }
+
+    @Override
+    public void update(chiTietHoaDon entity) {
+        jdbcHelper.executeUpdate(UPDATE_SQL, entity.getMaHoaDon(),entity.getSoLuong(),entity.getDonGia(),entity.getTongTien(), entity.getMaSanPham(), entity.getMaChiTietHoaDon());
+    }
+
+    @Override
+    public void delete(String maCTHD) {
+        jdbcHelper.executeUpdate(DELETE_SQL, maCTHD);
+    }
+
+    @Override
+    public List<chiTietHoaDon> selectAll() {
+        return selectBySql(SELECT_ALL_SQL);
+    }
+
+    @Override
+    public chiTietHoaDon selectById(String maCTHD) {
+        List<chiTietHoaDon> list = this.selectBySql(SELECT_BY_ID_SQL, maCTHD);
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.get(0);
+    }
+
+    @Override
+    protected List<chiTietHoaDon> selectBySql(String sql, Object... args) {
+     List<chiTietHoaDon> list = new ArrayList<>();
+        try {
+            ResultSet rs = jdbcHelper.executeQuery(sql, args);
+            while (rs.next()) {
+                chiTietHoaDon entity= new chiTietHoaDon();
+                entity.setMaChiTietHoaDon(rs.getString("MACHITIET"));
+                entity.setMaHoaDon(rs.getString("MAHD"));
+                entity.setSoLuong(rs.getInt("SOLUONG"));
+                entity.setDonGia(rs.getDouble("DONGIA"));
+                entity.setTongTien(rs.getDouble("TONGTIEN"));
+                entity.setMaSanPham(rs.getString("MASP"));
+                list.add(entity);
+            }
+            rs.getStatement().getConnection().close();
+            return list;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+}

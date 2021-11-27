7ab91016f5cfbcbcb4c2e2c1998427349d2c5774
tuentph17259 <tuentph17259@fpt.edu.vn>
2021-11-27 23:36:33 +0700

    thongKeDao

---
+++ src/Dao/thongKeDao.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dao;
+
+import Helper.jdbcHelper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author NGUYEN TRI TUE
+ */
+public class thongKeDao {
+    public List<Object[]> getDoanhThu(int nam) {
+        List<Object[]> list = new ArrayList<>();
+        try {
+            ResultSet rs = null;
+            try {
+                String sql = "{call sp_DoanhThu (?)}";
+                rs = jdbcHelper.executeQuery(sql, nam);
+                while (rs.next()) {
+                    Object[] model = {
+                        rs.getString("SANPHAM"),
+                        rs.getString("SOHD"),
+                        rs.getDouble("TONGDOANHTHU")
+                    };
+                    list.add(model);
+                }
+            } finally {
+                rs.getStatement().getConnection().close();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        }
+        return list;
+    }
+}

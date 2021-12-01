738f269a27bb6b1c8af9b8d01354242794d85e62
Duchvph17480 <duchvph17480@fpt.edu.vn>
2021-12-01 23:29:07 +0700

    done 3

--- src/View/phieuNhapInter.java
+++ src/View/phieuNhapInter.java
@@ -981,15 +981,20 @@ public class phieuNhapInter extends javax.swing.JInternalFrame {
         if (ct == null) {
             return;
         }
-        try {
-            ctDao.insert(ct);
-            this.xoaTrangCt();
-            this.LayDuLieuChiTietPhieuNhap(txtMaPN_ct.getText());
-            dialogHelper.alert(this, "thêm thành công");
-            SetTongTien();
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        
+        if (!txtTongTien.getText().equals("")) {
+            try {
+                ctDao.insert(ct);
+                this.xoaTrangCt();
+                this.LayDuLieuChiTietPhieuNhap(txtMaPN_ct.getText());
+                dialogHelper.alert(this, "thêm thành công");
+                SetTongTien();
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }else{
+            dialogHelper.alert(this, "bạn chưa tính tổng tiền");
         }
     }
 

3922518646144be18118a75e59f26e8feec5b711
hieuphtph17364 <80674964+kokkoro-prifes@users.noreply.github.com>
2021-12-16 20:56:34 +0700

    hieuphtph17364_cloneBranch_Test (#57)

--- src/Helper/jdbcHelper.java
+++ src/Helper/jdbcHelper.java
@@ -18,9 +18,9 @@ import java.sql.SQLException;
 public class jdbcHelper {
 
     public static String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-    public static String url = "jdbc:sqlserver://localhost\\DESKTOP-PQUBK1B\\SQLEXPRESS:1433;databaseName=DUAN1";
+    public static String url = "jdbc:sqlserver://localhost\\DESKTOP-HKEFBM1\\SQLEXPRESS:1433;databaseName=DUAN1";
     public static String username = "sa";
-    public static String password = "1234";
+    public static String password = "123456";
 
     //Nạp driver
     static {
